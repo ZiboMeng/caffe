70a3ddc9c3388a22756bc85df144d12fcfe8d200
ZiboMeng <mzbo1986@gmail.com>
2017-09-01 09:55:33 -0400

    new

--- run1_temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./examples/ckplus/scripts/siamese_bi_con_orig/create_protos.sh
-./examples/ckplus/scripts/siamese_bi_con_orig/train.sh
-
-./examples/ckplus/scripts/siamese_exp_con_orig/create_protos.sh
-./examples/ckplus/scripts/siamese_exp_con_orig/train.sh
-
--- run2_temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-./examples/ckplus/scripts/siamese_exp_con_work/create_protos.sh
-./examples/ckplus/scripts/siamese_exp_con_work/train.sh
-
-./examples/ckplus/scripts/siamese_bi_con_work/create_protos.sh
-./examples/ckplus/scripts/siamese_bi_con_work/train.sh
-
